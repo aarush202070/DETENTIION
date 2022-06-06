fdc2d66176565f3a4d731d8ec2003b946d9e6d25
aarush202070 <82890981+aarush202070@users.noreply.github.com>
2022-06-06 17:34:20 +0530

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,17 @@
+function addUser()
+{
+
+    window.location = "kwitter_room.html";
+}function addUsera()
+{
+
+    window.location = "kwitter_room_a.html";
+}function addUserb()
+{
+
+    window.location = "kwitter_room_b.html";
+}function addUserc()
+{
+
+    window.location = "kwitter_room_c.html";
+}
